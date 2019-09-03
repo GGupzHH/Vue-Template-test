dd06b875f6f2db7f802f84b80ca270ce4e9cd3af
GGupzhh <1248023357@qq.com>
2019-09-04 00:58:36 +0800

    NODE_ENV error

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,16 @@ module.exports = {
       errors: false
     }
   }
+  // configureWebpack: {
+  //   performance: {
+  //     hints: 'warning',
+  //     maxEntrypointSize: 50000000, // 入口起点的最大体积
+  //     // 生成文件的最大体积
+  //     maxAssetSize: 30000000,
+  //     // 只给出 js 文件的性能提示
+  //     assetFilter: function (assetFilename) {
+  //       return assetFilename.endsWith('.js')
+  //     }
+  //   }
+  // }
 }
