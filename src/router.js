c67250d839ac1ef35441f19a61cf51978aac69ec
GGupzhh <1248023357@qq.com>
2019-09-19 14:06:17 +0800

    Add routerbeforEach

--- src/router.js
+++ src/router.js
@@ -3,35 +3,43 @@ import Router from 'vue-router'
 import Index from './views/index.vue'
 
 Vue.use(Router)
-export default new Router({
+
+const constantRoutes = [
+  {
+    path: '/',
+    name: 'index',
+    component: Index
+  }, {
+    path: '/inputModel',
+    name: 'inputModel',
+    component: () => import('./components/for-model.vue')
+  }, {
+    path: '/upload',
+    name: 'upload',
+    component: () => import('./components/upload.vue')
+  }, {
+    path: '/eventBus',
+    name: 'eventBus',
+    component: () => import('./components/EventBus.vue')
+  }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: 'about' */ './views/About.vue')
+  // }
+]
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: Index
-    }, {
-      path: '/inputModel',
-      name: 'inputModel',
-      component: () => import('./components/for-model.vue')
-    }, {
-      path: '/upload',
-      name: 'upload',
-      component: () => import('./components/upload.vue')
-    }, {
-      path: '/eventBus',
-      name: 'eventBus',
-      component: () => import('./components/EventBus.vue')
-    }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: 'about' */ './views/About.vue')
-    // }
-  ]
+  routes: constantRoutes
 })
+// router.beforeEach
+router.beforeEach((to, from, next) => {
+  next()
+})
+export default router
