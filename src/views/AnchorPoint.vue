50d74dceff70e52af793bf4afce4e2d69ac5fdf9
GGupzhh <1248023357@qq.com>
2019-09-29 11:19:16 +0800

    联动锚点完成 锚点点击之后跳转不影响

--- src/views/AnchorPoint.vue
+++ src/views/AnchorPoint.vue
@@ -2,27 +2,29 @@
   <div class=''>
     <aside ref="aside">
       <ul>
-        <li v-for="(item, index) in aside_list" :key="index" :class="!index ? 'active' : ''" @click="getActive($event, index)">{{ item }}</li>
+        <li v-for="(item, index) in aside_list" :key="index" :class="item.active ? 'active' : ''" @click="getActive(index)">
+          <a :href=" '#anchor-' + index">{{ item.name }}</a>
+        </li>
       </ul>
     </aside>
   <main>
     <div class="box">
-      <div class="box-child">按钮1
+      <div class="box-child" id="anchor-0">按钮1
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
         <hr>
       </div>
-      <div class="box-child">按钮2
+      <div class="box-child" id="anchor-1">按钮2
         <br>
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
         <br>
         <br>
       </div>
-      <div class="box-child">按钮3
+      <div class="box-child" id="anchor-2">按钮3
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
         <br><br><br><br><br>
       </div>
-      <div class="box-child">按钮4
+      <div class="box-child" id="anchor-3">按钮4
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
       </div>
@@ -31,7 +33,9 @@
       </div>
       <div class="box-child">按钮6</div>
       <div class="box-child">按钮6</div>
-      <div class="box-child">按钮6</div>
+      <div class="box-child">
+        <router-link to="/">sssssssssssssssss</router-link>
+      </div>
     </div>
   </main>
   </div>
@@ -42,22 +46,51 @@
 export default {
   data() {
     return {
-      aside_list: ['按钮1', '按钮2','按钮3', '按钮4','按钮5', '按钮6','按钮7', '按钮8']
+      aside_list: [{
+        name: '按钮1',
+        active: 1
+      }, {
+        name: '按钮2',
+        active: 0
+      }, {
+        name: '按钮3',
+        active: 0
+      }, {
+        name: '按钮4',
+        active: 0
+      }, {
+        name: '按钮5',
+        active: 0
+      }, {
+        name: '按钮6',
+        active: 0
+      }]
     }
   },
   methods: {
-    getActive (e, index) {
-      // e = e || window.event
-      if (e.target.nodeName === 'LI') {
-        document.querySelector('.active').className = ''
-        e.target.className = 'active'
+    // getActive (e, index) {
+    //   // e = e || window.event
+    //   if (e.target.nodeName === 'LI') {
+    //     document.querySelector('.active').className = ''
+    //     e.target.className = 'active'
+    //   }
+    //   let boxChild = document.querySelectorAll('.box-child')
+    //   let total = boxChild[index].offsetTop
+    //   document.documentElement.scrollTop = total
+    //   window.pageYOffset = total
+    // },
+    mapFn (v) {
+      return {
+        name: v.name,
+        active: 0
       }
-      let boxChild = document.querySelectorAll('.box-child')
-      let total = boxChild[index].offsetTop
-      document.documentElement.scrollTop = total
-      window.pageYOffset = total
     },
-    // 防抖
+    getActive (index) {
+      let newAside_list = this.aside_list.map(this.mapFn)
+      newAside_list[index].active = 1
+      this.aside_list = newAside_list
+    },
+    //  需要改成防抖
     throttle (fn) {
       let on_off = true
       let that = this
@@ -70,6 +103,7 @@ export default {
         }, 500)
       }
     },
+    // 要一个负数一面最的
     onScroll () {
       // let scrolled = document.documentElement.scrollTop
       // console.log(scrolled)
@@ -80,28 +114,33 @@ export default {
       let activeItem = 0
       // console.log(minTop)
       for (let i = 0; i < boxChildAll.length; i++) {
-        // console.log(boxChildAll[i].getBoundingClientRect().top)
-        if (boxChildAll[i].getBoundingClientRect().top < 0) {
-          if (minTop > -boxChildAll[i].getBoundingClientRect().top) {
-            activeItem = i
-            minTop = minTop > -boxChildAll[i].getBoundingClientRect().top ? -boxChildAll[i].getBoundingClientRect().top : minTop
-          }
-        } else if (boxChildAll[i].getBoundingClientRect().top === 0) {
+        if (boxChildAll[i].getBoundingClientRect().top <= 0) {
           activeItem = i
-        } else {
-          if (minTop > boxChildAll[i].getBoundingClientRect().top) {
-            activeItem = i
-            minTop = minTop > boxChildAll[i].getBoundingClientRect().top ? boxChildAll[i].getBoundingClientRect().top : minTop
-          }
         }
+        // console.log(boxChildAll[i].getBoundingClientRect().top)
+        // if (boxChildAll[i].getBoundingClientRect().top < 0) {
+        //   if (minTop > -boxChildAll[i].getBoundingClientRect().top) {
+        //     activeItem = i
+        //     minTop = minTop > -boxChildAll[i].getBoundingClientRect().top ? -boxChildAll[i].getBoundingClientRect().top : minTop
+        //   }
+        // } else if (boxChildAll[i].getBoundingClientRect().top === 0) {
+        //   activeItem = i
+        // } else {
+        //   if (minTop > boxChildAll[i].getBoundingClientRect().top) {
+        //     activeItem = i
+        //     minTop = minTop > boxChildAll[i].getBoundingClientRect().top ? boxChildAll[i].getBoundingClientRect().top : minTop
+        //   }
+        // }
         // boxChildAll[i].hiegh
       }
       console.log(activeItem)
+      this.getActive(activeItem)
     }
   },
   mounted() {
     this.$nextTick(() => {
-      window.addEventListener('scroll', this.throttle(this.onScroll))
+      // window.addEventListener('scroll', this.throttle(this.onScroll))
+      window.addEventListener('scroll', this.onScroll)
     })
   }
 }
@@ -129,10 +168,18 @@ aside {
       padding-left: 20px;
       box-sizing: border-box;
       color: #000;
+      a {
+        display: block;
+        width: 100%;
+        height: 100%;
+        border-radius: 20px 0 0 20px;
+      }
     }
   }
-  li.active {
+  li.active > a{
     color: aqua;
+    background-color: rgb(255, 154, 22);
+        // border-radius: 50% 0 0 50%;
   }
 }
 main {
