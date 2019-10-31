c0f23047ffe77bb1c3859cdb1b1e3c47650548a0
GGupzhh <1248023357@qq.com>
2019-10-31 18:52:30 +0800

    updata vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,10 @@ module.exports = {
   // 设置为 true 时，eslint-loader 会将 lint 错误输出为编译警告。默认情况下，警告仅仅会被输出到命令行，且不会使得编译失败。
   // 如果你希望让 lint 错误在开发时直接显示在浏览器中，你可以使用 lintOnSave: 'error'。这会强制 eslint-loader 将 lint 错误输出为编译错误，同时也意味着 lint 错误将会导致编译失败。
   lintOnSave: true, // default:true
+  // 是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。
+  runtimeCompiler: true, // default false
+  // 默认情况下 babel-loader 会忽略所有 node_modules 中的文件。如果你想要通过 Babel 显式转译一个依赖，可以在这个选项中列出来。
+  transpileDependencies: [], // default []
   // configureWebpack: {
   //   performance: {
   //     hints: 'warning',
