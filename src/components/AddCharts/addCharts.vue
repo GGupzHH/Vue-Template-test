b584c9380e5229b73246e9e8682537ce2cce9e10
GGupzhh <1248023357@qq.com>
2020-07-22 14:31:56 +0800

    update(vue-component-table)

--- src/components/AddCharts/addCharts.vue
+++ src/components/AddCharts/addCharts.vue
@@ -99,7 +99,13 @@
             </div>
           </div>
           <div class="echarts-inside">
-
+            <v-table
+              :columns="columns"
+              :table-data="tableData"
+              even-bg-color="#f4f4f4"
+              row-hover-color="#eee"
+              row-click-color="#edf7ff"
+            ></v-table>
           </div>
         </div>
       </div>
@@ -121,7 +127,20 @@ export default {
       drawer: false,
       echartsName: '',
       echartsxAxisName: '',
-      echartsyAxisName: ''
+      echartsyAxisName: '',
+      tableData: [
+        {"name":"赵伟","tel":"156*****1987","hobby":"钢琴、书法、唱歌","address":"上海市黄浦区金陵东路569号17楼"},
+        {"name":"李伟","tel":"182*****1538","hobby":"钢琴、书法、唱歌","address":"上海市奉贤区南桥镇立新路12号2楼"},
+        {"name":"孙伟","tel":"161*****0097","hobby":"钢琴、书法、唱歌","address":"上海市崇明县城桥镇八一路739号"},
+        {"name":"周伟","tel":"197*****1123","hobby":"钢琴、书法、唱歌","address":"上海市青浦区青浦镇章浜路24号"},
+        {"name":"吴伟","tel":"183*****6678","hobby":"钢琴、书法、唱歌","address":"上海市松江区乐都西路867-871号"}
+      ],
+      columns: [
+        {field: 'name', title:'图例', width: 150, titleAlign: 'center',columnAlign:'center', isFrozen: true},
+        {field: 'tel', title: '一月', width: 280, titleAlign: 'center',columnAlign:'center'},
+        {field: 'hobby', title: '二月', width: 380, titleAlign: 'center',columnAlign:'center'},
+        {field: 'address', title: '三月', width: 430, titleAlign: 'center',columnAlign:'left'}
+      ]
     }
   },
   components: {},
--- src/main.js
+++ src/main.js
@@ -16,6 +16,14 @@ import echarts from 'echarts'
 import Wavebtn from './components/public/WaveBtn/WaveBtn.js'
 // import Normalize.css
 import 'normalize.css'
+// 引入样式
+import 'vue-easytable/libs/themes-base/index.css'
+// 导入 table 和 分页组件
+import {VTable,VPagination} from 'vue-easytable'
+
+// 注册到全局
+Vue.component(VTable.name, VTable)
+Vue.component(VPagination.name, VPagination)
 
 Vue.use(Wavebtn)
 Vue.use(api)
